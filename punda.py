47130d94d16ff3b3889c68412efb3c8d59b0f94f
sharuk <sharumathan8@gmail.com>
2025-12-24 13:03:14 +0530

    Commit Colab2

--- punda.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-model_name = "google/gemma-3-270m"
-
-tokenizer = AutoTokenizer.from_pretrained(model_name, token=True)
-model = AutoModelForCausalLM.from_pretrained(model_name, token=True)
-
-print("Gemma loaded successfully")
